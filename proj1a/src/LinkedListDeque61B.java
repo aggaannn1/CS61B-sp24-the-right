6cdb51d89ebdc03d74a800a9acaad5ae84198531
Yang Zhaoxiang <475018216@qq.com>
2025-11-17 22:30:27 +0800

    Linklist实现

--- proj1a/src/LinkedListDeque61B.java
+++ proj1a/src/LinkedListDeque61B.java
@@ -1,23 +1,57 @@
 import java.util.LinkedList;
 import java.util.List;
-
-public class LinkedListDeque61B<T> implements Deque61B<T>{
-    class akir{
-
+import java.util.ArrayList; // import the ArrayList class
+public class LinkedListDeque61B<T> implements Deque61B<T> {
+    Node sentinel;
+    int size = 0;
+    private class Node {
+        public T x;
+        public Node nextnode;
+        public Node lastnode;
+    }
+    public LinkedListDeque61B() {
+        this.sentinel = new Node();
+        sentinel.nextnode = sentinel;
+        sentinel.lastnode = sentinel;
     }
     @Override
     public void addFirst(T x) {
-
+        Node a = new Node();
+        a.x = x;
+        this.sentinel.nextnode.lastnode = a;
+        a.nextnode = this.sentinel.nextnode;
+        a.lastnode = this.sentinel;
+        this.sentinel.nextnode = a;
+        if (sentinel.nextnode.nextnode == sentinel){
+            this.sentinel.lastnode = a;
+        }
+        this.size++;
     }
 
     @Override
     public void addLast(T x) {
-
+        Node b = new Node();
+        b.x = x;
+        this.sentinel.lastnode.nextnode = b;
+        b.lastnode = this.sentinel.lastnode;
+        b.nextnode = this.sentinel;
+        this.sentinel.lastnode = b;
+        if (sentinel.nextnode.nextnode ==sentinel){
+            this.sentinel.lastnode = b;
+        }
+        this.size++;
     }
 
     @Override
     public List<T> toList() {
-        return List.of();
+        List<T> li = new ArrayList<T>();
+        Node k = this.sentinel.nextnode;
+        while (k != sentinel)
+        {
+            li.add(k.x);
+            k = k.nextnode;
+        }
+        return li;
     }
 
     @Override
