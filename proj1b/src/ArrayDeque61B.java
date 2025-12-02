254dbad153aa5bb335e3399c4a456357d42f1dff
Yang Zhaoxiang <475018216@qq.com>
2025-12-02 22:34:29 +0800

    添加 ArrayDeque61B.java 并修改测试文件

---
+++ proj1b/src/ArrayDeque61B.java
@@ -0,0 +1,127 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.lang.Math;
+public class ArrayDeque61B<T> implements Deque61B{
+
+    Object[] items = null;
+    int nextfirst;
+    int nextlast;
+    int size = 0;
+    int capacity = 8;
+    public ArrayDeque61B() {
+        this.items = (T[]) new Object[capacity];
+        this.nextfirst = 4;
+        this.nextlast = 5;
+    }
+    @Override
+    public void addFirst(Object x) {
+        if (size == capacity) {
+            Object[] items1 = (T[]) new Object[2*size];
+            for(int i = 1;i <= size;i++)
+            {
+                items1[i] = get(i);
+            }
+            nextfirst = 0;
+            nextlast = size+1;
+            items = items1;
+        }
+        items[nextfirst] = x;
+        nextfirst = Math.floorMod(nextfirst-1,capacity);
+        size++;
+    }
+
+    @Override
+    public void addLast(Object x) {
+        if (size == capacity) {
+            Object[] items1 = (T[]) new Object[2*size];
+            for(int i = 1;i <= size;i++)
+            {
+                items1[i] = get(i);
+            }
+            nextfirst = 0;
+            nextlast = size+1;
+            items = items1;
+        }
+        items[nextlast] = x;
+        nextlast = Math.floorMod(nextlast+1,capacity);
+        size++;
+    }
+
+    @Override
+    public List toList() {
+        List<Object> li = new ArrayList<>();
+        int i = Math.floorMod(nextfirst+1,capacity);
+        if (size == 0)
+        {
+            return li;
+        }
+        while (i != nextlast)
+        {
+            li.add(items[i]);
+            i++;
+            i = Math.floorMod(i,capacity);
+        }
+        return li;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        if (size == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int size() {
+
+        return size;
+    }
+
+    @Override
+    public Object removeFirst() {
+        if(size<0.25*capacity) {
+            Object[] items1 = (T[]) new Object[size/3];
+            for(int i = 1;i <= size;i++)
+            {
+                items1[i] = get(i);
+            }
+            nextfirst = 0;
+            nextlast = size+1;
+            items = items1;
+        }
+        nextfirst = Math.floorMod(++nextfirst,capacity);
+        items[nextfirst] = 0;
+        size--;
+        return null;
+    }
+
+    @Override
+    public Object removeLast() {
+        if(size<0.25*capacity) {
+            Object[] items1 = (T[]) new Object[size/3];
+            for(int i = 1;i <= size;i++)
+            {
+                items1[i] = get(i);
+            }
+            nextfirst = 0;
+            nextlast = size+1;
+            items = items1;
+        }
+        nextlast = Math.floorMod(--nextlast,capacity);
+        items[nextlast] = 0;
+        size--;
+        return null;
+    }
+
+    @Override
+    public Object get(int index) {
+        int i = Math.floorMod(nextfirst+index,capacity);
+        return items[i];
+    }
+
+    @Override
+    public T getRecursive(int index) {
+        throw new UnsupportedOperationException("No need to implement getRecursive for proj 1b");
+    }
+}
--- proj1b/tests/ArrayDeque61BPreconditionTest.java
+++ proj1b/tests/ArrayDeque61BPreconditionTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.lang.reflect.Field;
 import java.util.List;
 
+import static com.google.common.truth.Truth.assertThat;
 import static com.google.common.truth.Truth.assertWithMessage;
 
 public class ArrayDeque61BPreconditionTest {
@@ -17,5 +18,79 @@ public class ArrayDeque61BPreconditionTest {
                 .toList();
 
         assertWithMessage("Found fields that are not array or primitives").that(badFields).isEmpty();
+    }
+    @Test
+    void addFirstTest() {
+        Deque61B<Integer> all1 = new ArrayDeque61B<>();
+        all1.addFirst(1);
+        all1.addFirst(2);
+        all1.addFirst(3);
+        all1.addFirst(4);
+        all1.addFirst(5);
+        all1.addFirst(6);
+        all1.addFirst(7);
+        assertThat(all1.toList()).containsExactly(7,5,4,3,2,1);
+    }
+    @Test
+    void addLastTest() {
+        Deque61B<Integer> all1 = new ArrayDeque61B<>();
+        all1.addFirst(1);
+        all1.addFirst(2);
+        all1.addFirst(3);
+        all1.addFirst(4);
+        all1.addFirst(5);
+        all1.addFirst(6);
+        all1.addFirst(7);
+        assertThat(all1.toList()).containsExactly(1,2,3,4,5,6,7);
+    }
+    @Test
+    void isEmptyTest() {
+        Deque61B<Integer> all2 = new ArrayDeque61B<>();
+        assertThat(all2.isEmpty()).isTrue();
+        all2.addFirst(1);
+        all2.addFirst(2);
+        all2.addFirst(3);
+        assertThat(all2.isEmpty()).isFalse();
+    }
+    @Test
+    void sizeTest() {
+        Deque61B<Integer> all2 = new ArrayDeque61B<>();
+        assertThat(all2.size()).isEqualTo(0);
+        all2.addFirst(1);
+        all2.addFirst(2);
+        all2.addFirst(3);
+        assertThat(all2.size()).isEqualTo(3);
+    }
+    @Test
+    public void removeFirstTest() {
+        Deque61B<Integer> lld1 = new ArrayDeque61B<>();
+        lld1.addLast(0);   // [0]
+        lld1.addLast(1);   // [0, 1]
+        lld1.addFirst(-1); // [-1, 0, 1]
+        lld1.addLast(2);   // [-1, 0, 1, 2]
+        lld1.addFirst(-2); // [-2, -1, 0, 1, 2]
+        lld1.removeFirst();
+        assertThat(lld1.toList()).containsExactly(-1,0,1,2);
+    }
+    @Test
+    public void removeLastTest() {
+        Deque61B<Integer> lld1 = new ArrayDeque61B<>();
+        lld1.addLast(0);   // [0]
+        lld1.addLast(1);   // [0, 1]
+        lld1.addFirst(-1); // [-1, 0, 1]
+        lld1.addLast(2);   // [-1, 0, 1, 2]
+        lld1.addFirst(-2); // [-2, -1, 0, 1, 2]
+        lld1.removeLast();
+        assertThat(lld1.toList()).containsExactly(-2,-1,0,1);
+    }
+    @Test
+    public void getTest() {
+        Deque61B<Integer> lld1 = new ArrayDeque61B<>();
+        lld1.addLast(0);   // [0]
+        lld1.addLast(1);   // [0, 1]
+        lld1.addFirst(-1); // [-1, 0, 1]
+        lld1.addLast(2);   // [-1, 0, 1, 2]
+        lld1.addFirst(-2); // [-2, -1, 0, 1, 2]
+        assertThat(lld1.get(3)).isEqualTo(0);
     }
 }
